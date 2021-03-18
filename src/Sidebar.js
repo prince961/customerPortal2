f09d00ef5ff97b4dbd8b1a930dce5bcb96705bb0
mohitpanwar <prince.prince961@gmail.com>
2021-03-18 07:59:52 +0530

    form data converted to JSON

---
+++ src/Sidebar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {Navigation} from 'react-minimal-side-navigation';
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+
+
+
+const Sidebar = () => {
+    return (
+      <nav className="sidebar">
+      <>
+
+      <Navigation
+          // you can use your own router's api to get pathname
+          activeItemId="/management/members"
+          onSelect={({itemId}) => {
+            // maybe push to the route
+      
+          }}
+          
+          items={[
+            {
+              title: 'Dashboard',
+              itemId: '/dashboard',
+              // you can use your own custom Icon component as well
+              // icon is optional
+              //elemBefore: () => <Icon name="inbox" />,
+            },
+            {
+              title: 'Management',
+              itemId: '/management',
+              //elemBefore: () => <Icon name="users" />,
+              subNav: [
+                {
+                  title: 'Projects',
+                  itemId: '/management/projects',
+                },
+                {
+                  title: 'Members',
+                  itemId: '/management/members',
+                },
+              ],
+            },
+            {
+              title: 'Another Item',
+              itemId: '/another',
+              subNav: [
+                {
+                  title: 'Teams',
+                  itemId: '/management/teams',
+                },
+              ],
+            },
+          ]}
+        />
+    </>
+    </nav>
+
+
+
+
+    );
+}
+ 
+export default Sidebar;
